ec80d81b07084b03ace2a86f177c183318fdb7c7
priyasbarua12 <ppriyasbarua55@gmail.com>
2026-08-18 22:29:17 +0600

    python submit

--- turtle-tree.py
+++ turtle-tree.py
@@ -1,11 +1,11 @@
 import turtle
 import colorsys
 
-def draw_fractal_crystal():
+def draw_fractal_tree():
     screen = turtle.Screen()
     screen.bgcolor("black")
     screen.setup(width=800, height=800)
-    screen.title("Ultra-Lajawab Python Fractal Effect")
+    screen.title("turtle Tree")
     screen.tracer(50)
 
     t = turtle.Turtle()
@@ -20,14 +20,14 @@ def draw_fractal_crystal():
         if branch_len < 5:
             return
         
-        color = colorsys.hsv_to_rgb(hue, 1, 1)
+        color = "#680d91"
         t.pencolor(color)
         t.width(branch_len / 10)
         
         t.forward(branch_len)
         
         new_len = branch_len * 0.75
-        new_hue = (hue + 0.1) % 1.0
+        new_hue = (hue + 0.05) % 1.0
         
         t.right(angle)
         draw_branch(new_len, angle, new_hue)
@@ -40,19 +40,11 @@ def draw_fractal_crystal():
         t.backward(branch_len)
         t.down()
 
-    print("Drawing the Neon Fractal Crystal...")
-    draw_branch(120, 25, 0.0)
-    
-    t.up()
-    t.goto(0, 0)
-    t.setheading(0)
-    for i in range(36):
-        t.pencolor(colorsys.hsv_to_rgb(i/36, 1, 1))
-        t.circle(200, 10)
-        t.dot(10)
+    print("Drawing the fractal tree...")
+    draw_branch(120, 25, 0.33)  # hue 0.33 = green tree
 
     print("Complete! Click the window to exit.")
     screen.exitonclick()
 
 if __name__ == "__main__":
-    draw_fractal_crystal()
+    draw_fractal_tree()
